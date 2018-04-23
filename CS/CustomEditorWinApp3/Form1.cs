0439c65bf9e6c1f401055cc403e592ffcf959d3f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CustomEditorWinApp3/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+
+namespace CustomEditor
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, System.EventArgs e)
+		{
+			this.categoriesTableAdapter.Fill(this.nwindDataSet.Categories);
+			this.productsTableAdapter.Fill(this.nwindDataSet.Products);
+		}
+	}
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to make LookUpEdit set its EditValue to null when its Text is an empty string
+
+
+<p>This sample demonstrates how to make LookUpEdit set its  EditValue to null when its Text property is set to an empty string</p>
+
+<br/>
+
+
